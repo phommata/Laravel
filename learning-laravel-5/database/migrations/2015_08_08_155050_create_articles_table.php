5003b186aaf93e2df9de986b8156ae16117e2d3d
phommata <phommata@onid.oregonstate.edut>
2015-08-08 08:57:18 -0700

    Modify articles table
    homestead ssh php artisan migrate

--- learning-laravel-5/database/migrations/2015_08_08_155050_create_articles_table.php
+++ learning-laravel-5/database/migrations/2015_08_08_155050_create_articles_table.php
@@ -14,7 +14,10 @@ class CreateArticlesTable extends Migration
     {
         Schema::create('articles', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('title');
+            $table->text('body');
             $table->timestamps();
+            $table->timestamps('published_at');
         });
     }
 
