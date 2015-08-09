568a8493d3a588a984977a571326ed17ca45b2c8
phommata <phommata@onid.oregonstate.edut>
2015-08-09 02:34:20 -0700

    9 foreach article

--- learning-laravel-5/resources/views/articles/index.blade.php
+++ learning-laravel-5/resources/views/articles/index.blade.php
@@ -4,4 +4,14 @@
 
     <h1>Articles</h1>
 
+    @foreach ($articles as $article)
+
+        <article>
+
+            <h2>{{ $article->title }}</h2>
+
+            <div class="body">{{ $article->body }}</div>
+
+        </article>
+    @endforeach
 @stop
