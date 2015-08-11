773e49515ba806b580ed543a1a6be53720660839
phommata <phommata@onid.oregonstate.edu>
2015-08-11 01:28:31 -0700

    10 Form input Name
    Bootstrapped

--- learning-laravel-5/resources/views/articles/create.blade.php
+++ learning-laravel-5/resources/views/articles/create.blade.php
@@ -8,6 +8,14 @@
 
     {!! Form::open() !!}
 
+        <div class="form-group">
+
+            {!! Form::label('name', 'Name: ') !!}
+
+            {!! Form::text('name', null, ['class' => 'form-control']) !!}
+
+        </div>
+
     {!! Form::close() !!}
 
 @stop
