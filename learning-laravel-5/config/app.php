3ca3c6cf342b07edccb405d73fd1ff99ef6f0507
phommata <phommata@onid.oregonstate.edu>
2015-08-11 01:21:55 -0700

    10 laravelcollective/html: 5.1.*

--- learning-laravel-5/config/app.php
+++ learning-laravel-5/config/app.php
@@ -137,7 +137,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-//        Collective\Html\HtmlServiceProvider::class,
+        Collective\Html\HtmlServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -193,8 +193,8 @@ return [
         'URL'       => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View'      => Illuminate\Support\Facades\View::class,
-//        'Form'      => Collective\Html\FormFacade::class,
-//        'Html'      => Collective\Html\HtmlFacade::class,
+        'Form'      => Collective\Html\FormFacade::class,
+        'Html'      => Collective\Html\HtmlFacade::class,
 
     ],
 
