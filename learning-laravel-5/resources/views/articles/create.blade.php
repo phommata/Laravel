ff32b5f9bff9d154454492d63407cb136ea5e875
phommata <phommata@onid.oregonstate.edu>
2015-08-11 02:02:53 -0700

    10 Form text, text area, submit

--- learning-laravel-5/resources/views/articles/create.blade.php
+++ learning-laravel-5/resources/views/articles/create.blade.php
@@ -10,11 +10,29 @@
 
         <div class="form-group">
 
-            {!! Form::label('name', 'Name: ') !!}
+            {!! Form::label('title', 'Title: ') !!}
 
-            {!! Form::text('name', null, ['class' => 'form-control']) !!}
+            {!! Form::text('title', null, ['class' => 'form-control']) !!}
 
         </div>
+
+        <!-- Body Form Input -->
+
+        <div class="form-group">
+
+            {!! Form::label('body', 'Body: ') !!}
+
+            {!! Form::textarea('body', null, ['class' => 'form-control']) !!}
+
+        </div>
+
+    <!-- Add Article Form Input -->
+
+    <div class="form-group">
+
+        {!! Form::submit('Add Article', ['class' => 'btn btn-primary form-control']) !!}
+
+    </div>
 
     {!! Form::close() !!}
 
