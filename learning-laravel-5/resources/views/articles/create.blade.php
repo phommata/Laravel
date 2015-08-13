4879f41d480553d9cbd8ef91fd81da47959435e6
phommata <phommata@onid.oregonstate.edu>
2015-08-13 16:00:39 -0700

    errors list.blade.php
    @include('errors/list')

--- learning-laravel-5/resources/views/articles/create.blade.php
+++ learning-laravel-5/resources/views/articles/create.blade.php
@@ -46,18 +46,6 @@
 
     {!! Form::close() !!}
 
-    @if($errors->any())
-
-        <ul class="alert alert-danger">
-
-            @foreach ($errors->all() as $error)
-
-                <li>{{ $error }}</li>
-
-            @endforeach
-
-        </ul>
-
-    @endif
+    @include('errors/list')
 
 @stop
