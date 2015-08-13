3018e1732c6ad91801e67ef43119e17fd6d3a5bb
phommata <phommata@onid.oregonstate.edu>
2015-08-13 15:11:23 -0700

    13 Edit Article

--- learning-laravel-5/app/Http/routes.php
+++ learning-laravel-5/app/Http/routes.php
@@ -20,12 +20,11 @@ Route::get('/', 'WelcomeController@index');
 Route::get('about', 'PagesController@about');
 Route::get('contact', 'PagesController@contact');
 
-Route::get('home', 'HomeController@index');
-
 Route::get('articles', 'ArticlesController@index');
 Route::get('articles/create', 'ArticlesController@create');
 Route::get('articles/{id}', 'ArticlesController@show');
 Route::post('articles', 'ArticlesController@store');
+Route::get('articles/{id}/edit','ArticlesController@edit');
 
 //Route::controllers([
 //    'auth', 'Auth\AuthController',
