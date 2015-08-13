a61f9a0d7ed07401d2feffe207a75dd037ec4455
phommata <phommata@onid.oregonstate.edu>
2015-08-13 15:24:38 -0700

    13 Edit url

--- learning-laravel-5/app/Http/Controllers/ArticlesController.php
+++ learning-laravel-5/app/Http/Controllers/ArticlesController.php
@@ -48,4 +48,11 @@ class ArticlesController extends Controller
         return redirect('articles');
 
     }
+
+    public function edit($id){
+
+        $article = Article::findOrFail($id);
+
+        return view('articles.edit', compact('article'));
+    }
 }
