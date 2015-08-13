c016788ad657fb090d4d6de9d47ffb2676436551
phommata <phommata@onid.oregonstate.edu>
2015-08-13 15:17:09 -0700

    13 Remove boiler plate
    in favor of route resource

--- learning-laravel-5/app/Http/routes.php
+++ learning-laravel-5/app/Http/routes.php
@@ -11,24 +11,9 @@
 |
 */
 
-//Route::get('/', function () {
-//    return view('welcome');
-//});
-
 Route::get('/', 'WelcomeController@index');
 
 Route::get('about', 'PagesController@about');
 Route::get('contact', 'PagesController@contact');
-//
-//Route::get('articles', 'ArticlesController@index');
-//Route::get('articles/create', 'ArticlesController@create');
-//Route::get('articles/{id}', 'ArticlesController@show');
-//Route::post('articles', 'ArticlesController@store');
-//Route::get('articles/{id}/edit','ArticlesController@edit');
-
-//Route::controllers([
-//    'auth', 'Auth\AuthController',
-//    'password', 'Auth\PasswordController',
-//]);
 
 Route::resource('articles', 'ArticlesController');
