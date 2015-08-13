45e46454a1181ac64f9505cf4cc1a06d7639eadd
phommata <phommata@onid.oregonstate.edu>
2015-08-13 15:12:22 -0700

    13 Laravel generate routes automatically
    Route::resource('articles', 'ArticlesController');

--- learning-laravel-5/app/Http/routes.php
+++ learning-laravel-5/app/Http/routes.php
@@ -19,14 +19,16 @@ Route::get('/', 'WelcomeController@index');
 
 Route::get('about', 'PagesController@about');
 Route::get('contact', 'PagesController@contact');
-
-Route::get('articles', 'ArticlesController@index');
-Route::get('articles/create', 'ArticlesController@create');
-Route::get('articles/{id}', 'ArticlesController@show');
-Route::post('articles', 'ArticlesController@store');
-Route::get('articles/{id}/edit','ArticlesController@edit');
+//
+//Route::get('articles', 'ArticlesController@index');
+//Route::get('articles/create', 'ArticlesController@create');
+//Route::get('articles/{id}', 'ArticlesController@show');
+//Route::post('articles', 'ArticlesController@store');
+//Route::get('articles/{id}/edit','ArticlesController@edit');
 
 //Route::controllers([
 //    'auth', 'Auth\AuthController',
 //    'password', 'Auth\PasswordController',
 //]);
+
+Route::resource('articles', 'ArticlesController');
