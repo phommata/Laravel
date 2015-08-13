af0970ee982700e7ed973dbe31dc77d796ad51ad
phommata <phommata@onid.oregonstate.edu>
2015-08-13 00:13:18 -0700

    11 published_at <= now

--- learning-laravel-5/app/Http/Controllers/ArticlesController.php
+++ learning-laravel-5/app/Http/Controllers/ArticlesController.php
@@ -15,7 +15,7 @@ class ArticlesController extends Controller
     public function index(){
 
 //        $articles = Article::order_by('published_at', 'desc')->get();
-        $articles = Article::latest('published_at')->get();
+        $articles = Article::latest('published_at')->where('published_at', '<=', Carbon::now())->get();
 
         return view('articles.index', compact('articles'));
 //        return view('articles.index')->with('articles', $articles);
