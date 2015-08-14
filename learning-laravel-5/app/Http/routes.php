597d9820ecd4dd80af66b53c6bf8824733ebb130
phommata <phommata@onid.oregonstate.edu>
2015-08-14 01:49:00 -0700

    15 auth/login routes and dir

--- learning-laravel-5/app/Http/routes.php
+++ learning-laravel-5/app/Http/routes.php
@@ -17,3 +17,12 @@ Route::get('about', 'PagesController@about');
 Route::get('contact', 'PagesController@contact');
 
 Route::resource('articles', 'ArticlesController');
+
+// Authentication routes...
+Route::get('auth/login', 'Auth\AuthController@getLogin');
+Route::post('auth/login', 'Auth\AuthController@postLogin');
+Route::get('auth/logout', 'Auth\AuthController@getLogout');
+
+// Registration routes...
+Route::get('auth/register', 'Auth\AuthController@getRegister');
+Route::post('auth/register', 'Auth\AuthController@postRegister');
