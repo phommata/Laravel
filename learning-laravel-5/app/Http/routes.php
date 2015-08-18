43c47135c7553328ba0ab9ebd202aea0d581b524
phommata <phommata@onid.oregonstate.edu>
2015-08-18 14:28:02 -0700

    16 Can attach middleware at route level

--- learning-laravel-5/app/Http/routes.php
+++ learning-laravel-5/app/Http/routes.php
@@ -13,7 +13,7 @@
 
 Route::get('/', 'WelcomeController@index');
 
-Route::get('about', 'PagesController@about');
+Route::get('about', ['middleware' => 'auth', 'uses' => 'PagesController@about']);
 Route::get('contact', 'PagesController@contact');
 
 Route::resource('articles', 'ArticlesController');
