11e7df33442992b4bd78ebc553e4e1b2b84144e1
phommata <phommata@onid.oregonstate.edu>
2015-08-18 14:24:10 -0700

    16 Attach Authenticate middleware to only create
    Instead of attaching Authenticate middleware to every route, attach to
    only create

--- learning-laravel-5/app/Http/Controllers/ArticlesController.php
+++ learning-laravel-5/app/Http/Controllers/ArticlesController.php
@@ -15,7 +15,7 @@ class ArticlesController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('auth');
+        $this->middleware('auth', ['only' => 'create']);
     }
 
     public function index(){
