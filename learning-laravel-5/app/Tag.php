bd47c7e8832e5df8d9560645d1a4722dd14258b9
phommata <phommata@onid.oregonstate.edu>
2015-08-20 14:45:30 -0700

    21 All articles assoc. with the given tag

--- learning-laravel-5/app/Tag.php
+++ learning-laravel-5/app/Tag.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Tag extends Model
 {
-    //
+    // Want the inverse, so if you have a tag object, $tag->article. Give me all articles associated with the given tag
+    public function articles()
+    {
+        return $this->belongsToMany('App\Article');
+    }
 }
