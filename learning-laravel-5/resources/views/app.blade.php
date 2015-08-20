ec9df6e7fbc0ecca0d1edd3bf0627aa1c8f0736d
phommata <phommata@onid.oregonstate.edu>
2015-08-20 01:46:54 -0700

    19 Elixir function
    Reads rev-manifest.json file and figure out all.css corresponds with
    version and that’s what we reference

--- learning-laravel-5/resources/views/app.blade.php
+++ learning-laravel-5/resources/views/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <title>Document</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-    <link rel="stylesheet" href="/css/app.css">
+    <link rel="stylesheet" href="{{ elixir('css/all.css') }}">
 </head>
 <body>
     <div class="container">
