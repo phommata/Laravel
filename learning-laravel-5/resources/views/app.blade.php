67317dbb23c693993be44c203f37d05827e01e47
phommata <phommata@onid.oregonstate.edu>
2015-08-20 01:42:45 -0700

    19 Handle from view
    While hardcoding css in

--- learning-laravel-5/resources/views/app.blade.php
+++ learning-laravel-5/resources/views/app.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <title>Document</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/css/app.css">
 </head>
 <body>
     <div class="container">
