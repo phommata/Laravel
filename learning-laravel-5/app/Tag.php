60fb728610b369e4b5980ce0969e89d806697522
phommata <phommata@onid.oregonstate.edu>
2015-08-20 14:48:39 -0700

    21 Pass unconventionally named table name as second argument

--- learning-laravel-5/app/Tag.php
+++ learning-laravel-5/app/Tag.php
@@ -9,6 +9,10 @@ class Tag extends Model
     // Want the inverse, so if you have a tag object, $tag->article. Give me all articles associated with the given tag
     public function articles()
     {
-        return $this->belongsToMany('App\Article');
+        return $this->belongsToMany('App\Article', "tags_pivot"); // if you don't want to follow conventions, you maybe
+                                                                  // already working with something, or maybe you don't
+                                                                  // want to follow the convention, just pass it through
+                                                                  // as the second argument. Ex: tags_pivot. That will
+                                                                  // be treated as the table name.
     }
 }
