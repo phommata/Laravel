f857475744d58394af09962853c9c1c77d555fc7
phommata <phommata@onid.oregonstate.edu>
2015-08-20 14:54:29 -0700

    21 Third and fourth arg for non_standard column name like article_identifier for article id

--- learning-laravel-5/app/Tag.php
+++ learning-laravel-5/app/Tag.php
@@ -6,13 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Tag extends Model
 {
-    // Want the inverse, so if you have a tag object, $tag->article. Give me all articles associated with the given tag
+    // Want the inverse, so if you have a tag object, $tag->article.
+    // Give me all articles associated with the given tag
+
     public function articles()
     {
-        return $this->belongsToMany('App\Article', "tags_pivot"); // if you don't want to follow conventions, you maybe
-                                                                  // already working with something, or maybe you don't
-                                                                  // want to follow the convention, just pass it through
-                                                                  // as the second argument. Ex: tags_pivot. That will
-                                                                  // be treated as the table name.
+        // if you don't want to follow conventions, you maybe are
+        // already working with something, or maybe you don't
+        // want to follow the convention, just pass it through
+        // as the second argument. Ex: tags_pivot. That will
+        // be treated as the table name. Can pass a third or
+        // fourth argument for a non-standard name for the
+        // article id like article_identifier. Same is true for
+        // the tag id.
+        return $this->belongsToMany('App\Article', "tags_pivot", "article_identifier");
     }
 }
