18033973c449820e2449f451efe537e613bc2128
phommata <phommata@onid.oregonstate.edu>
2015-08-22 02:03:08 -0700

    25 link_to_route

--- learning-laravel-5/resources/views/app.blade.php
+++ learning-laravel-5/resources/views/app.blade.php
@@ -25,7 +25,7 @@
                 </ul>
 
                 <ul class="nav navbar-nav navbar-right">
-                    <li>{!! link_to_action !!}</li>
+                    <li>{!! link_to_route !!}</li>
                 </ul>
             </div><!--/.nav-collapse -->
         </div>
