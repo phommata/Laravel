0afe3b09fd79503e30ddb4f2611a0bcfc76913fe
phommata <phommata@onid.oregonstate.edu>
2015-08-24 08:42:27 -0700

    26 Resolve conflicts

--- learning-laravel-5/app/Http/routes.php
+++ learning-laravel-5/app/Http/routes.php
@@ -15,6 +15,7 @@ Route::get('foo', 'FooController@foo');
 
 //Route::get('/', 'WelcomeController@index');
 
+
 Route::get('about', 'PagesController@about');
 Route::get('contact', 'PagesController@contact');
 
