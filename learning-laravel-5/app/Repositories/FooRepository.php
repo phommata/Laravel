983b392d9dc2bdbdb077ae257c8923ab3f1a25cd
phommata <phommata@onid.oregonstate.edu>
2015-08-24 01:29:54 -0700

    26 get method returns an array of items

---
+++ learning-laravel-5/app/Repositories/FooRepository.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: andrewphommathep
+ * Date: 8/24/15
+ * Time: 1:28 AM
+ */
+
+class FooRepository {
+
+    public function get()
+    {
+        return ['array', 'of', 'items'];
+    }
+}
