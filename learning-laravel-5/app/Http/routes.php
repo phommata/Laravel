316f81362afc4f5db2e1d4296e714b2cab26c0fa
phommata <phommata@onid.oregonstate.edu>
2015-08-24 09:40:16 -0700

    25 Put back ViewComposerServiceProvider.php
    Composers/NavigationComposer.php is not neccessary.

--- learning-laravel-5/app/Http/routes.php
+++ learning-laravel-5/app/Http/routes.php
@@ -15,7 +15,6 @@ Route::get('foo', 'FooController@foo');
 
 //Route::get('/', 'WelcomeController@index');
 
-
 Route::get('about', 'PagesController@about');
 Route::get('contact', 'PagesController@contact');
 
--- learning-laravel-5/app/Providers/ViewComposerServiceProvider.php
+++ learning-laravel-5/app/Providers/ViewComposerServiceProvider.php
@@ -35,9 +35,9 @@ class ViewComposerServiceProvider extends ServiceProvider
         // Not pass a closure, pass a class path as a string
         // Behind the scenes, Laravel will detect the attempt to resolve a class out of a service container
         // and will trigger a specific method on it.
-        view()->composer('partials.nav', 'App\Http\Composers\NavigationComposer');
-//        view()->composer('partials.nav', function ($view) {
-//            $view->with('latest', Article::latest()->first());
-//        });
+//        view()->composer('partials.nav', 'App\Http\Composers\NavigationComposer');
+        view()->composer('partials.nav', function ($view) {
+            $view->with('latest', Article::latest()->first());
+        });
     }
 }
